2559ca3b797409a40f8a8fcd39e994b3b9d4b2c8
DevHackingS2 <87339066+DevHackingS2@users.noreply.github.com>
2021-09-11 23:47:51 -0300

    Add files via upload

---
+++ 1607.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import time
+
+os.system("clear")
+
+os.system("nano mariah.txt")
+
+os.system("clear")
+
+aviso = "Nesse texto, você terá que ser paciente, Obrigado =/ Te amo s2"
+
+for char in aviso:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+os.system("clear")
+
+message = """=| Bom dia ou boa noite vida! |= 
+= Hoje infelizmente vamos de textinho simples, não é porque eu não quero e sim o sono, provavelmente até as 00:00 eu não acordei ainda,
+= Espero que você durma bem e acorde bem, te amo muito, tô com saudades, uma das coisas que eu falo para você sempre é que se você estivesse comigo estaria muito melhor, 
+= Mas infelizmente isso não é possível, tem outra coisa também, ontem fiquei meio chateado não sei se percebeu, mas tinha ficado por causa da Nikoly (aquela p*ta :) ), na minha vida inteira nunca aconteceu isso
+= e agora ela ficar com essa palhaçada, gosto dela como amiga, mas não tem oque fazer a não ser corta o mal pela raiz, infelizmente... Mas enfim, não fiz esse textinho com esse intuito, até porque como você disse
+= um bom relacionamento funciona com um bom dialogo, não exatamente se falou isso, mas deu a entender isso, enfim... Sinceramente, eu fico lembrando do nosso primeiro encontro, queria poder voltar no tempo,
+= só para ver aquele filme ruim junto com você novamente, você não sabe o quão bem você me faz, você reformulou (não sei se escreve assim) minha cabeça totalmente, meu jeito de pensar, várias coisas, sou muito grato por isso
+= uma coisa que eu não quero tanto para mim como para você, é terminar com você, você me faz tão bem com aqueles textinhos, com as suas fotos, dos nossos encontros, vocẽ sempre será a minha princesa, sempre será a pessoa eu amarei sempre,
+= é dificil dizer em palavras o quão eu gosto de você tanto como amiga, namorada, esposa o que for, um tempo atrás, você tinha dito do porque eu tinha certeza que era você, não queria te dar exatamente a resposta porque estava aguardando para outro momento,
+= porque eu nunca falei tanto de alguém pro Gabriel, igual eu falo de você, eu até brinco com o Gabriel falando que o cupido tava meio vesgo, mirou na Maria Eduarda e acertou você, mas enfim... É muito dificil não poder te ver, tu não sabe o quanto eu fico esperando pra dar o Anel de pato que você me pediu, fora outras ideias que eu quero fazer com você, você sabia que só você faz minha vida andar? acho que não, mas enfim... Quero que lembre, eu sempre vou estar aqui pra te apoiar em tudo que for fazer, até porque o meu mundo já é chato, com você nele vira a 8º Maravilha do Mundo...
+
+
+
+
+
+
+=| Te amo, Tô com saudade! |=
+                    (Feito pelo seu Namoradinho! )
+"""
+for char in message:
+    sys.stdout.write(char)
+    sys.stdout.flush
+    time.sleep(0.1)
